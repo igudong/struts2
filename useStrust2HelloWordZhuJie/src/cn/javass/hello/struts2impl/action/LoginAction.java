df3163f5e7fcc0fe331ebfd48fc2399cba8fac45
igudong <281254712@qq.com>
2016-03-15 14:32:47 +0800

    1

--- useStrust2HelloWordZhuJie/src/cn/javass/hello/struts2impl/action/LoginAction.java
+++ useStrust2HelloWordZhuJie/src/cn/javass/hello/struts2impl/action/LoginAction.java
@@ -10,6 +10,20 @@ import com.opensymphony.xwork2.ActionSupport;
 
 @Namespace("/servletimpl")
 @Result(name="success",location="/servletimpl/login.jsp")
+/*
+ * @Result(name="success",location="servletimpl/login.jsp")
+ * 找的是路径(/useStrust2HelloWord/WEB-INF/content/
+ * servletimpl/servletimpl/login.jsp)是这个，是/WEB-INF/content/下的
+ * 加上namespace和location
+ * 
+ * */
+
+/*
+ * @Result(name="success",location="/servletimpl/login.jsp")
+ * 找的是路径(useStrust2HelloWordZhuJie/servletimpl/login.jsp)是这个，
+ * 是根目录下的文件
+ * 
+ * */
 public class LoginAction extends ActionSupport{
 
 
