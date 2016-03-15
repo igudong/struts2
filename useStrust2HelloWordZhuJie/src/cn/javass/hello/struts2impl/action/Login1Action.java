33d241288c8312c91d4b305d3e990037cec7cd68
igudong <281254712@qq.com>
2016-03-15 14:45:05 +0800

    1

--- useStrust2HelloWordZhuJie/src/cn/javass/hello/struts2impl/action/Login1Action.java
+++ useStrust2HelloWordZhuJie/src/cn/javass/hello/struts2impl/action/Login1Action.java
@@ -12,6 +12,15 @@ import com.opensymphony.xwork2.ActionSupport;
 @Namespace("/servletimpl")
 @ResultPath("/test")
 @Result(name="success",location="happy.jsp")
+/*
+ * 
+ * 当@Result注解中location的写的是绝对路径的时候，
+ * @ResultPath的注解不起作用
+ * 
+ * 当@Result注解中location的写的是相对路径的时候，
+ * @ResultPath的注解起作用，但是路径等于@ResultPath的路径
+ * 加上@Namespace注解的路径，最后再加上@Result的location的路径
+ * */
 public class Login1Action extends ActionSupport{
 
 
